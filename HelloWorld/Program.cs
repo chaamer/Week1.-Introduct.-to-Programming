40746825013f97d24d5b8f0450631c1559978c32
Carl <chaamer@tktk.ee>
2022-02-07 16:07:03 +0200

    Visual Studio 22 HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+string UserName;
+UserName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + UserName);
+
+Console.Read();
+
